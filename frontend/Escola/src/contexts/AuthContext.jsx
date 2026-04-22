b6848d34992501e30251a8cbfb13e264dff03f11
GBZINH006 <gabrieldossantosdutra06@gmail.com>
2026-04-22 11:16:14 -0300

    css

--- frontend/Escola/src/contexts/AuthContext.jsx
+++ frontend/Escola/src/contexts/AuthContext.jsx
@@ -1,1 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import { Children, createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const login = (data) => setUser(data);
+    const logout = () => setUser(null);
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
