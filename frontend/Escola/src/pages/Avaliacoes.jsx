a20a0e8fe0eb0d8dbbc3fcd6d7dca0069cefb1f4
ana697024 <ana_pv_farias@estudante.sesisenai.org.br>
2026-05-02 11:08:05 -0300

    excluindo coisas inúteis

--- frontend/Escola/src/pages/Avaliacoes.jsx
+++
@@ -1,8 +0,0 @@
-export default function Avaliacoes() {
-    return (
-      <div>
-        <h1>Notas</h1>
-        <p>Lançamento de notas</p>
-      </div>
-    );
-  }
--- frontend/Escola/src/pages/Painel.jsx
+++
@@ -1,8 +0,0 @@
-export default function Painel() {
-    return (
-      <div>
-        <h1>Painel Admin</h1>
-        <p>Resumo geral do sistema</p>
-      </div>
-    );
-  }
